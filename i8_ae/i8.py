3395bb4a265b3d85dd642f5bf51cb428b997d30d
Shahab Cypher <shahabcypher@gmail.com>
2026-09-24 10:51:35 +0330

    added bearer prefix and rate limit crash

--- i8_ae/i8.py
+++ i8_ae/i8.py
@@ -1,19 +1,22 @@
-import sys
-import json
 import requests
 
 class i8():
 	def __init__(self, api_key):
 		self.key = api_key
-	
+
 	# link shortener
 	def short(self, url, password=None):
 		if self.key == None:
 			raise Exception("Please add API KEY first: i8 = i8(key)")
-			
+
 		else:
+			# API requires "Bearer <key>" (https://i8.ae/developers)
+			key = str(self.key)
+			if not key.lower().startswith("bearer "):
+				key = "Bearer " + key
+
 			headers = {
-				'Authorization': self.key,
+				'Authorization': key,
 				'Content-Type': 'application/json'
 			}
 			
@@ -33,8 +36,12 @@ class i8():
 			)
 			
 			# getting i8.ae response
-			response = url_req.json()
-			
+			try:
+				response = url_req.json()
+			except ValueError:
+				# non-JSON body, e.g. hitting the 30 req/min rate limit
+				raise Exception("HTTP %s: %s" % (url_req.status_code, url_req.text[:200]))
+
 			if response['error'] != 0:
 				message = response['message']
 				raise Exception(message)
